c2b8bb94ba33bb5132df330627c70e03e943b345
CsomosAron <csomosaa@edu.bme.hu>
2024-10-16 09:23:13 +0200

    Added comments for the generator variables

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,7 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
-  private Random generator = new Random();
+  private Random generator = new Random();  // create a random value for the overheating of the launcher bay
 
   public TorpedoStore(int numberOfTorpedos){
     this.torpedoCount = numberOfTorpedos;
